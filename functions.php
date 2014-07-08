14746fbf211cbfb1f5d58ca6d1136f987569f061
Ben Dolly <bromeodolly@gmail.com>
2014-07-08 15:14:33 -0400

    INITIAL SCSS/ STRUCTURE AND CONFIG.RB - responsive scss file structure and styles borrow copied from Bones theme (https://github.com/eddiemachado/bones) - stripped out style in top-level style.css - setup scss/config.rb to reflect file structure - enquequed stylesheets in funcitons.php -> _s_scripts()
    TODO:
    - comb through scss and delete or comment out unwanted styles
    - enqueque Modernizr and either JQuery or Zepto, preferably from a CDN
    - setup Grunt task and Bower branch

--- functions.php
+++ functions.php
@@ -65,7 +65,40 @@ add_action( 'widgets_init', '_s_widgets_init' );
  * Enqueue scripts and styles.
  */
 function _s_scripts() {
-	wp_enqueue_style( '_s-style', get_stylesheet_uri() );
+	global $wp_styles; // call global $wp_styles variable to add conditional wrapper around ie stylesheet the WordPress way
+
+  if (!is_admin()) {
+
+		// modernizr (without media query polyfill)
+		// wp_register_script( '_s-modernizr', get_stylesheet_directory_uri() . '/js/libs/modernizr.custom.min.js', array(), '2.5.3', false );
+
+		// register main stylesheet
+		wp_register_style( '_s-stylesheet', get_stylesheet_directory_uri() . '/css/style.css', array(), '', 'all' );
+
+		// ie-only style sheet
+		wp_register_style( '_s-ie-only', get_stylesheet_directory_uri() . '/css/ie.css', array(), '' );
+
+		//adding scripts file in the footer
+		wp_register_script( '_s-js', get_stylesheet_directory_uri() . '/js/scripts.js', array( 'jquery' ), '', true );
+
+		// enqueue styles and scripts
+		// wp_enqueue_script( '_s-modernizr' );
+		wp_enqueue_style( '_s-stylesheet' );
+		wp_enqueue_style( '_s-ie-only' );
+
+		$wp_styles->add_data( '_s-ie-only', 'conditional', 'lt IE 9' ); // add conditional wrapper around ie stylesheet
+
+		/*
+		I recommend using a plugin to call jQuery
+		using the google cdn. That way it stays cached
+		and your site will load faster.
+		*/
+		// wp_enqueue_script( 'jquery' );
+		// wp_enqueue_script( '_s-js' );
+
+	}
+
+	// wp_enqueue_style( '_s-style', get_stylesheet_uri() );
 }
 add_action( 'wp_enqueue_scripts', '_s_scripts' );
 
